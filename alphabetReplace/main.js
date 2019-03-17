a8ecd1e01489dce5ffcd59d0794178e66c3bce86
Nyxist <parker.tech9@gmail.com>
2019-03-17 14:00:24 -0400

    alphabetReplace finished. Moving on to either one of the unfinished problems or starting an easier one.

--- alphabetReplace/main.js
+++ alphabetReplace/main.js
@@ -3,20 +3,20 @@
 //ex: "a" =  1, "b" = 2 ...     "aba" = "1 2 1"
 
 var test = "This is a test";
-// var patt = /a-z/gi;
+var valueArray = [];
 
 function alphabetPosition(text) {
 
   var alphabetArray = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   text = text.toLowerCase();
   for(i = 0; i < text.length; i++){
-    for(j = 0; i < alphabetArray.length; j++){
+    for(j = 0; j < alphabetArray.length; j++){
       if(text[i] == alphabetArray[j]){
-        text[i] = alphabetArray.indexOf(j);
+        valueArray.push(j + 1);
       }
     }
   }
-  console.log(text);
+  return valueArray.join(" ");
 }
 
 alphabetPosition(test);
